47146b713de47aab8e352840cde7372c78691eae
Geekid812 <geekid812@gmail.com>
2022-06-30 10:54:59 +0200

    Synchronize player status

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,56 @@
 import process = require("process");
 import { JsonMap } from "@iarna/toml";
 
-import { XmlRPCInterface } from "./xmlrpc";
+import { RPCPlayer, XmlRPCInterface } from "./xmlrpc";
 import { isProduction } from "./live";
 
-export type GameServer = {
-    name: string,
-    login: string,
-    xmlrpc: XmlRPCInterface
+export class GameServer {
+    name: string;
+    login: string;
+    xmlrpc: XmlRPCInterface;
+    players: RPCPlayer[];
+    spectators: RPCPlayer[];
+
+    constructor(name, login, xmlrpc) {
+        this.name = name;
+        this.login = login;
+        this.xmlrpc = xmlrpc;
+        this.players = [];
+        this.spectators = [];
+
+        xmlrpc.gbx.on("callback", async (method, params) => this.callback(method, params));
+        this.init();
+    }
+
+    private async init() {
+        let players = await this.xmlrpc.getPlayers();
+        this.players = players.filter((p) => p.SpectatorStatus === 0);
+        this.spectators = players.filter((p) => p.SpectatorStatus !== 0);
+        console.log(this.players, this.spectators);
+    }
+
+    private async callback(method: string, params: any) {
+        switch (method) {
+            case "ManiaPlanet.PlayerInfoChanged":
+                let player: RPCPlayer = params[0];
+                this.forget(player.Login);
+
+                if (player.SpectatorStatus === 0) {
+                    this.players.push(player);
+                } else {
+                    this.spectators.push(player);
+                }
+                break;
+            case "ManiaPlanet.PlayerDisconnect":
+                this.forget(params[0]);
+                break;
+        }
+    }
+
+    private forget(login: string) {
+        this.players.splice(this.players.findIndex((p) => p.Login == login), 1);
+        this.spectators.splice(this.spectators.findIndex((p) => p.Login == login), 1);
+    }
 }
 
 export async function loadServers(config: JsonMap): Promise<GameServer[]> {
@@ -24,7 +67,7 @@ export async function loadServers(config: JsonMap): Promise<GameServer[]> {
 
         promises.push(XmlRPCInterface.connect(host, port, username, password).then((xmlrpc) => {
             console.log(`connection to '${name}' succeeded`);
-            servers.push({ name, login, xmlrpc });
+            servers.push(new GameServer(name, login, xmlrpc));
         }));
     })
     await Promise.all(promises);
--- src/xmlrpc.ts
+++ src/xmlrpc.ts
@@ -1,5 +1,7 @@
 import { GbxClient } from "@evotm/gbxclient";
 
+// Use the same version as PyPlanet
+const apiVersion = '2013-04-16';
 
 export class XmlRPCInterface {
     gbx: GbxClient;
@@ -12,7 +14,30 @@ export class XmlRPCInterface {
         const client = new GbxClient();
         await client.connect(host, port);
         await client.call("Authenticate", name, pass);
-        
+        await client.multicall([
+            ["SetApiVersion", apiVersion],
+            ["EnableCallbacks", true]
+        ]);
+        await client.callScript("XmlRpc.EnableCallbacks", "true");
+
         return new XmlRPCInterface(client);
     }
+
+    public async getPlayers(): Promise<RPCPlayer[]> {
+        let res: RPCPlayer[] = await this.gbx.call("GetPlayerList", -1, 0);
+
+        // Filter out the server account
+        return res.filter((p) => p.PlayerId !== 0);
+    }
+}
+
+export type RPCPlayer = {
+    Login: string,
+    NickName: string,
+    PlayerId: number,
+    TeamId: number,
+    SpectatorStatus: number,
+    LadderRanking: number,
+    Flags: number,
+    LadderScore: number
 }
